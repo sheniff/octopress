d4e2bada603ea801ee96a8c44f04cc3314b88821
Sergio Almecija <sergio.almecija@tapjoy.com>
2013-03-27 15:58:46 -0700

    Trying to put the block up and left... still not working...

--- source/javascripts/landing.js
+++ source/javascripts/landing.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+  var whichTransitionEvent = function(){
+    var t, el = document.createElement('fakeelement'),
+        transitions = {
+          'transition':'transitionend',
+          'OTransition':'oTransitionEnd',
+          'MozTransition':'transitionend',
+          'WebkitTransition':'webkitTransitionEnd'
+        };
+
+    for(t in transitions)
+      if( el.style[t] !== undefined )
+        return transitions[t];
+  };
+
   var tiles = {
     $container: $('#content'),
     $tiles: $('#content .tile'),
@@ -13,51 +27,66 @@ $(document).ready(function(){
       for(i = 0; i < tiles.length; i++)
         $(tiles[i]).addClass('delay'+Math.ceil(Math.random()*4));
       tiles.removeClass('loading');
-      this.enableSelection();
       return true;
     },
 
     enableSelection: function(){
       var self = this;
+
       this.$container.on('click', '.tile.interactive', function(e){
         e.preventDefault();
+        e.stopPropagation();
 
-        var $target = $(e.currentTarget),
-            transitionEnd = self.whichTransitionEvent();
+        var $el = $(this);
 
-        $target.addEventListener(transitionEnd, function() {
-          self.showTheBlock($target);
-        }, false);
+        theBlock.place($el);
 
-        $target.removeClass('delay1 delay2 delay3 delay4').addClass('selected');
-      })
+        $el
+          .removeClass('delay1 delay2 delay3 delay4')
+          .addClass('selected');
+
+        theBlock.show();
+      });
+    }
+  };
+
+/////////
+
+  var theBlock = {
+    block: $('#the_block'),
+
+    place: function($ref) {
+      this.block
+        .height($ref.height())
+        .width($ref.width())
+        .offset($ref.offset())
+        .addClass('growing');
     },
 
-    whichTransitionEvent: function(){
-      var t, el = document.createElement('fakeelement'),
-          transitions = {
-            'transition':'transitionend',
-            'OTransition':'oTransitionEnd',
-            'MozTransition':'transitionend',
-            'WebkitTransition':'webkitTransitionEnd'
-          };
+    show: function() {
+      var self = this;
+      this.block.one(whichTransitionEvent(), function() {
+        self.expand();
+      });
 
-      for(t in transitions)
-        if( el.style[t] !== undefined )
-          return transitions[t];
+      this.block.removeClass('hidden');
     },
 
-    showTheBlock: function($ref) {
-      var $theblock = $('#the_block');
+    hide: function() {
+      this.block.addClass('hidden');
+    },
 
-      $theblock.height($ref.height());
-      $theblock.width($ref.width());
-      $theblock.offset($ref.offset());
-
-      $theblock.removeClass('hidden');
+    expand: function($block) {
+      this.block
+        .offset({
+          top: 0,
+          left: 0
+        })
+        .width(760)
+        .height(570);
     }
 
-  }
+  };
 
   // Init
   tiles.show();
