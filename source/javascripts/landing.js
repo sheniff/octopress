4a430c5900e7efcf0320e9ce19b1b4b9f1c5e118
Sergio Almecija <sergio.almecija@tapjoy.com>
2013-03-27 10:35:59 -0700

    making the block appear in the right place

--- source/javascripts/landing.js
+++ source/javascripts/landing.js
@@ -1,25 +1,65 @@
 $(document).ready(function(){
 
-  var presentation = {
+  var tiles = {
+    $container: $('#content'),
+    $tiles: $('#content .tile'),
 
-    showTiles: function(){
+    start: function() {
+      this.show();
+    },
+
+    show: function(){
       var tiles = $('.loading'), i;
       for(i = 0; i < tiles.length; i++)
         $(tiles[i]).addClass('delay'+Math.ceil(Math.random()*4));
       tiles.removeClass('loading');
+      this.enableSelection();
       return true;
     },
 
-    loadSelect: function(){
-      $('#content').on('click', '.tile.interactive', function(e){
+    enableSelection: function(){
+      var self = this;
+      this.$container.on('click', '.tile.interactive', function(e){
         e.preventDefault();
-        $(e.currentTarget).removeClass('delay1 delay2 delay3 delay4').toggleClass('selected');
+
+        var $target = $(e.currentTarget),
+            transitionEnd = self.whichTransitionEvent();
+
+        $target.addEventListener(transitionEnd, function() {
+          self.showTheBlock($target);
+        }, false);
+
+        $target.removeClass('delay1 delay2 delay3 delay4').addClass('selected');
       })
+    },
+
+    whichTransitionEvent: function(){
+      var t, el = document.createElement('fakeelement'),
+          transitions = {
+            'transition':'transitionend',
+            'OTransition':'oTransitionEnd',
+            'MozTransition':'transitionend',
+            'WebkitTransition':'webkitTransitionEnd'
+          };
+
+      for(t in transitions)
+        if( el.style[t] !== undefined )
+          return transitions[t];
+    },
+
+    showTheBlock: function($ref) {
+      var $theblock = $('#the_block');
+
+      $theblock.height($ref.height());
+      $theblock.width($ref.width());
+      $theblock.offset($ref.offset());
+
+      $theblock.removeClass('hidden');
     }
 
   }
 
   // Init
-  presentation.showTiles();
-  presentation.loadSelect();
+  tiles.show();
+  tiles.enableSelection();
 });
